f84ced143c31bf4fd9d6e59d6184fe968f3464d9
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-03-06 22:15:50 +0700

    commit crud react redux

---
+++ src/components/AddProduct.js
@@ -0,0 +1,54 @@
+// MENGIRIM DATA
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { saveProduct } from "../features/productSlice";
+import { useNavigate } from "react-router-dom";
+
+const AddProduct = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const createProduct = async (e) => {
+    e.preventDefault();
+    await dispatch(saveProduct({ title, price }));
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <form className="box mt-5" onSubmit={createProduct}>
+        <div className="field">
+          <label className="label">Title</label>
+          <div className="control">
+            <input
+              type="text"
+              className="input"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Price</label>
+          <div className="control">
+            <input
+              type="text"
+              className="input"
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-success">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
